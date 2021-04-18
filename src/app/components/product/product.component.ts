91ed709f77b78c6597fcf2af6aa8126290af8079
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-04-19 02:22:18 +0400

    data from API

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http'
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+
+  products: Product[]= [];
+  //productResponseModel:ProductResponseModel = {};
+    
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("Init isledi");
+  }
+
+  getProducts(){
+    
+  }
+
+}
