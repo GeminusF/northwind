76c639d2f9e53f788dc6e043c3beac5afb5b3f71
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-04-23 16:13:51 +0400

    jquery added, event bubbling, root

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ProductResponseModel } from '../models/productResponseModel';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  apiUrl = 'https://localhost:44332/api/Products/getall';
+  
+  apiUrl = 'https://localhost:44332/api/';
+
 
   constructor(private httpClient: HttpClient) {}
 
-  getProducts(): Observable<ProductResponseModel> {
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  getProducts(): Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "Products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number): Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "Products/getbycategory?categoryId=" + categoryId 
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 }
