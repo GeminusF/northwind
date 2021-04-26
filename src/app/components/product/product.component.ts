5b1cc26aff3e93f828f42d0a34cd1127a7fc3945
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-04-26 15:24:35 +0400

    pipe and toastr added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -11,22 +13,24 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
+  filterText = '';
 
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
     this.getProducts();
-    this.activatedRoute.params.subscribe((params)=>{
-      if (params["categoryId"]) {
-        this.getProductsByCategory(params["categoryId"]);
-      }else{
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
         this.getProducts();
       }
-      
-    })
+    });
   }
 
   getProducts() {
@@ -36,10 +40,19 @@ export class ProductComponent implements OnInit {
     });
   }
 
-  getProductsByCategory(categoryId:number) {
-    this.productService.getProductsByCategory(categoryId).subscribe((response) => {
-      this.products = response.data;
-      this.dataLoaded = true;
-    });
+  getProductsByCategory(categoryId: number) {
+    this.productService
+      .getProductsByCategory(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
   }
+
+  addToCart(product: Product) {
+    this.toastrService.success('Səbətə əlavə olundu', product.productName);
+    this.cartService.addToCart(product);
+  }
+
+
 }
